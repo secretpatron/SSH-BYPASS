8501446045c6136a7aeaa12d0d6fc308570480e9
PATRON <78246461+5hahram@users.noreply.github.com>
2024-12-13 19:29:40 +0000

    Add files via upload

---
+++ connect_ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if tmux session exists
+if ! tmux has-session -t ssh_session 2>/dev/null; then
+  tmux new-session -d -s ssh_session "sshpass -p 'segfault' ssh -L5900:0:5900 -o 'SetEnv SECRET=YOUR SECRET KEY' root@lulz.segfault.net"
+fi
+
+# Only attach session if running in a terminal
+if [ -t 1 ]; then
+  tmux attach-session -t ssh_session
+fi
