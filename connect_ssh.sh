44c705348ba0ca6f91ef441cf09bb88b2a79728f
PATRON <78246461+5hahram@users.noreply.github.com>
2024-12-15 10:27:28 +0330

    Update connect_ssh.sh

--- connect_ssh.sh
+++ connect_ssh.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Check if tmux session exists
 if ! tmux has-session -t ssh-session 2>/dev/null; then
   echo "Starting new tmux session for SSH..."
-  # ایجاد یک session جدید tmux برای SSH
   sshpass -p 'segfault' ssh -o 'SetEnv SECRET=8lgm-Nhwod2sFV1tDZNNWuXPBx8Ll' root@lulz.segfault.net
 else
   echo "tmux session already exists. Attaching..."
-  # اگر session موجود بود، به آن متصل می‌شود
   tmux attach-session -t ssh-session
 fi
