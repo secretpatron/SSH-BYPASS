378ef14b4d8fede5ad0d1999940bbcc3b7ead8f3
PATRON <78246461+5hahram@users.noreply.github.com>
2024-12-23 14:39:50 +0000

    Update connect_ssh.sh

--- connect_ssh.sh
+++ connect_ssh.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Check if tmux session exists
+# بررسی اینکه آیا جلسه tmux وجود دارد
 if ! tmux has-session -t ssh-session 2>/dev/null; then
   echo "Starting new tmux session for SSH..."
-  
-  sshpass -p 'segfault' ssh -o 'SetEnv SECRET=YOUR_SECRET_KEY' root@lulz.segfault.net
+
+  # اتصال به سرور SSH با تلاش مجدد در صورت شکست
+  while true; do
+    sshpass -p "${SSH_PASSWORD}" ssh -o 'SetEnv SECRET=IKchmmqmqWXCmHKqmTEDilqd' -o ServerAliveInterval=60 -o ServerAliveCountMax=3 root@lulz.segfault.net
+    echo "SSH connection lost. Retrying in 10 seconds..."
+    sleep 10
+  done | tmux new-session -d -s ssh-session
 else
   echo "tmux session already exists. Attaching..."
   
