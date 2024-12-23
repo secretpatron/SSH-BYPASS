ce776b03836fd05b6b5aeb26c60dd53dac8ec36d
PATRON <78246461+5hahram@users.noreply.github.com>
2024-12-23 08:28:35 +0000

    Update connect_ssh.sh

--- connect_ssh.sh
+++ connect_ssh.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Check if tmux session exists
-if ! tmux has-session -t ssh_session 2>/dev/null; then
-  tmux new-session -d -s ssh_session "sshpass -p 'segfault' ssh -X -L5900:0:5900 -o 'SetEnv SECRET=IKchmmqmqWXCmHKqmTEDilqd' root@lulz.segfault.net"
-fi
-
-# Only attach session if running in a terminal
-if [ -t 1 ]; then
-  tmux attach-session -t ssh_session
+if ! tmux has-session -t ssh-session 2>/dev/null; then
+  echo "Starting new tmux session for SSH..."
+  
+  sshpass -p 'segfault' ssh -o 'SetEnv SECRET=YOUR_SECRET_KEY' root@lulz.segfault.net
+else
+  echo "tmux session already exists. Attaching..."
+  
+  tmux attach-session -t ssh-session
 fi
