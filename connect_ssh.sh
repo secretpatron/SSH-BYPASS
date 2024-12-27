6b8eeec4941abd2559d2ede3305e3c9719f41873
PATRON <78246461+5hahram@users.noreply.github.com>
2024-12-27 12:05:08 +0000

    Update connect_ssh.sh

--- connect_ssh.sh
+++ connect_ssh.sh
@@ -4,7 +4,7 @@ if ! tmux has-session -t ssh-session 2>/dev/null; then
   echo "Starting new tmux session for SSH..."
 
   while true; do
-    sshpass -p "segfault" ssh -L5900:localhost:5900 -o 'SetEnv SECRET=IKchmmqmqWXCmHKqmTEDilqd' -o ServerAliveInterval=60 -o ServerAliveCountMax=3 root@lulz.segfault.net "sudo apt-get update -y && sudo --autoremove purge"
+    sshpass -p "segfault" ssh -L5900:localhost:5900 -o 'SetEnv SECRET=IKchmmqmqWXCmHKqmTEDilqd' -o ServerAliveInterval=60 -o ServerAliveCountMax=3 root@lulz.segfault.net "sudo apt-get update -y && sudo --autoremove purge && bash /root/ok.sh"
     echo "SSH connection lost. Retrying in 10 seconds..."
     sleep 10
   done | tmux new-session -d -s ssh-session
